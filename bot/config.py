d04a97c13eddb0f7a56aa663342ac30e87beea82
Arsenic-23 <156888342+Arsenic-23@users.noreply.github.com>
2025-04-05 00:36:56 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "your_bot_token_here"
+BOT_TOKEN = "7575567291:AAH8yaPhRofVfAFrDPiEqddelnqszgQ5Ru8"
 WEB_APP_URL = "https://your-frontend-domain.com"  # your deployed frontend (or localhost)
